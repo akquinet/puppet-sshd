d4c727114c4110c23c4b99480bc12f7605c95803
SHaselbauer <sarah.haselbauer@akquinet.de>
2012-06-20 14:06:31 +0200

    added notify service attr to File definition sshd_config

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,17 +12,20 @@
 #
 # [Remember: No empty lines between comments and class definition]
 class sshd ($port ='22'){
-	package { "openssh-server":
-		ensure => present,
-	}
-	file { "/etc/ssh/sshd_config":
-		ensure => present,
-		content => template('sshd/sshd_config.erb'),
-		require => Package['openssh-server']
-	}
-	service { "sshd":
-		ensure => running,
-		restart => true,
-		require => File["/etc/ssh/sshd_config"]
-	}
+        package { "openssh-server":
+                ensure => present,
+        }
+        file { "/etc/ssh/sshd_config":
+                ensure => present,
+                content => template('sshd/sshd_config.erb'),
+                require => Package['openssh-server'],
+                notify => Service['sshd']
+        }
+        service { "sshd":
+                ensure => running,
+                hasrestart => true,
+                hasstatus => true,
+                enable => true,
+                require => File["/etc/ssh/sshd_config"]
+        }
 }
